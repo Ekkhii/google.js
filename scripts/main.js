d5358c86a91a787ff59e0ac7fd1aeef6aeed9db9
Martin Berangere <martin.berangere@live.fr>
2018-04-05 17:59:09 +0200

    Finalisation

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+function masquerLaPage(){
+    document.getElementById('corps_page').style.display = 'none';
+}
+
+function afficherLaPage(){
+    document.getElementById('corps_page').style.display = 'block';
+    document.getElementById('button').style.display = 'none';
+}
+
+function popup() {
+    setTimeout(function(){ 
+        alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); 
+    }, 10000);
+}
